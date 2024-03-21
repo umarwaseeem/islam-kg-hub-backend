9df8f7a33fca90da4a7375bb49a2ef03dfbcb733
Umar-Waseem <umar.waseem@gmail.com>
2024-03-22 02:02:12 +0500

    initial commit, demo api

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from SPARQLWrapper import SPARQLWrapper, JSON
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+repoURL = "http://192.168.10.7:7200/repositories/fyp-graph-db-1234"
+
+rawiName = "عبد الله بن يوسف@ar"
+def createQuery():
+
+    query = f"""
+    PREFIX : <http://www.semantichadith.com/ontology/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    select (COUNT (?name) AS ?num)
+    where 
+    {{ 
+        ?hadith rdf:type :Hadith .
+        ?hadith :hasNarratorChain ?o .
+        ?o :hasNarratorSegment	 ?x .
+        ?x :refersToNarrator+	 ?y .
+        ?y :name ?name
+        
+    }} 
+    VALUES (?name)
+    {{
+        ("{rawiName}")
+    }}
+    """
+
+    return query
+
+def queryGraphDB(query):
+    sparql = SPARQLWrapper(repoURL)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+
+    results = sparql.query().convert()
+    return results["results"]["bindings"]
+
+# post route
+@app.route('/userQuery', methods=['GET'])
+def userQuery():
+    query = createQuery()
+    results = queryGraphDB(query)
+    num = results[0]["num"]["value"]
+    answer = f"Narrator has narrated {num} hadiths"
+    response = {
+        "response": answer
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
